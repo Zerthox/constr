1b810fe04d1f796d7916e063b61676c568dc90a1
Zerthox <zerthox@gmail.com>
2020-05-20 21:27:04 +0200

    Implement comparison operators

--- src/constr.hpp
+++ src/constr.hpp
@@ -5,7 +5,7 @@ template <std::size_t N>
 struct constr : std::array<char, N> {
 	private:
 		template <std::size_t i = 0>
-		constexpr void _init(const char (&str)[N]) {
+		constexpr void _init(const char (&str)[N]) noexcept {
 			if constexpr (i < N) {
 				this->at(i) = str[i];
 				_init<i + 1>(str);
@@ -13,40 +13,63 @@ struct constr : std::array<char, N> {
 		}
 
 		template <std::size_t A, std::size_t B, std::size_t i = 0>
-		static constexpr void _concat(const char* a, const char* b, constr<A + B - 1>& result) {
+		static constexpr bool _compare(const char* a, const char* b) noexcept {
+			if constexpr (A != B) {
+				return false;
+			}
+			if constexpr (i < A) {
+				return a[i] == b[i] && _compare<A, B, i + 1>(a, b);
+			}
+			else {
+				return true;
+			}
+		}
+
+		template <std::size_t A, std::size_t B, std::size_t i = 0>
+		static constexpr void _concat(const char* a, const char* b, constr<A + B - 1>& result) noexcept {
 			if constexpr (i < A - 1) {
 				result[i] = a[i];
 				_concat<A, B, i + 1>(a, b, result);
 			}
-			else if constexpr (i < A + B - 1){
+			else if constexpr (i < A + B - 1) {
 				result[i] = b[i - A + 1];
 				_concat<A, B, i + 1>(a, b, result);
 			}
 		}
 
 	public:
-		constexpr constr() {}
+		constexpr constr() noexcept {}
 
-		constexpr constr(const char (&str)[N]) {
+		constexpr constr(const char (&str)[N]) noexcept {
 			_init(str);
 		}
 
-		constexpr const char* raw() const {
+		constexpr const char* raw() const noexcept {
 			return this->data();
 		}
 
-		constexpr std::size_t size() const {
+		constexpr std::size_t size() const noexcept {
 			return N;
 		}
 
 		template <std::size_t M>
-		constexpr constr<N + M - 1> operator+(const constr<M>& other) const {
+		constexpr bool operator==(const constr<M>& other) const noexcept {
+			return _compare<N, M>(raw(), other.raw());
+		}
+
+		template <std::size_t M>
+		constexpr bool operator!=(const constr<M>& other) const noexcept {
+			return !operator==<M>(other);
+		}
+
+		template <std::size_t M>
+		constexpr constr<N + M - 1> operator+(const constr<M>& other) const noexcept {
 			constr<N + M - 1> result;
-			_concat<N, M, 0>(raw(), other.raw(), result);
+			_concat<N, M>(raw(), other.raw(), result);
 			return result;
 		}
 
-		friend std::ostream& operator<<(std::ostream& os, const constr<N>& str) {
+		friend std::ostream& operator<<(std::ostream& os, const constr<N>& str) noexcept {
 			os << str.raw();
 			return os;
 		}
--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,9 @@
 #include "../src/constr.hpp"
 
 constexpr constr a{"abc"};
-constexpr constr b{"def"};
+constexpr constr b{"de"};
+constexpr constr c{"xyz"};
+constexpr constr d{"abc"};
 
 int main() {
 	static_assert(a.size() == 4, "Invalid constr size");
@@ -11,13 +13,15 @@ int main() {
 	static_assert(a.raw()[2] == 'c', "Invalid constr.raw character at position 2");
 	static_assert(a.raw()[3] == '\0', "Invalid constr.raw character at position 3");
 	static_assert(a[0] == 'a', "Invalid constr.operator[] result");
-	static_assert((a + b).size() == 7, "Invalid constr.operator+ result size");
+	static_assert((a + b).size() == 6, "Invalid constr.operator+ result size");
 	static_assert((a + b)[0] == 'a', "Invalid constr.operator+ result character at position 0");
 	static_assert((a + b)[1] == 'b', "Invalid constr.operator+ result character at position 1");
 	static_assert((a + b)[2] == 'c', "Invalid constr.operator+ result character at position 2");
 	static_assert((a + b)[3] == 'd', "Invalid constr.operator+ result character at position 3");
 	static_assert((a + b)[4] == 'e', "Invalid constr.operator+ result character at position 4");
-	static_assert((a + b)[5] == 'f', "Invalid constr.operator+ result character at position 5");
-	static_assert((a + b)[6] == '\0', "Invalid constr.operator+ result character at position 6");
+	static_assert((a + b)[5] == '\0', "Invalid constr.operator+ result character at position 6");
+	static_assert(a == d, "Invalid constr.operator== result with same operands");
+	static_assert(a != c, "Invalid constr.operator== result with different operands with same length");
+	static_assert(a != b, "Invalid constr.operator== result with different operands with different length");
 	std::cout << "All tests successful." << std::endl;
 }
